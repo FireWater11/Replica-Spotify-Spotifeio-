465c069c9aaa65b8b9c312d7b9f79323306f1541
FireWater11 <luidi.augusto1234@gmail.com>
2025-08-19 19:46:14 -0300

    commit enviroment configurando inicial

--- src/enviroment/enviroment.ts
+++ src/enviroment/enviroment.ts
@@ -1,3 +1,20 @@
 export  const  enviroment = {
     production: false
 }
+
+export const SpotifyConfiguration = {
+    clientId: 'e9ec27c674584a2b91fdba0556b24d9c',
+    authEndPoint: 'https://accounts.spotify.com/authorize',
+    apiTokenEndPoint: 'https://accounts.spotify.com/api/token',
+    redirectUrl: 'http://127.0.0.1:4200/login',
+    scopes: [
+        'user-read-currently-playing', // Musica que esta tocando
+        'user-read-recently-played', // Musicas que foram tocadas recentemente
+        'user-read-playback-state', // Estado da reproduçao
+        'user-top-read', // Top artistas e musicas
+        'user-modify-playback-state', // Controlar a reproduçao
+        'user-library-read', // Ler biblioteca de musicas
+        'playlist-read-private', // Ler playlists privadas
+        'playlist-read-collaborative' // Ler playlists colaborativas
+    ]
+}
