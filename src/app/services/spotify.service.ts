c719340e37f45f05b747adc22f422376c01bab5e
FireWater11 <luidi.augusto1234@gmail.com>
2025-08-19 22:28:26 -0300

    adicionando metodos obterurllogin

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -9,8 +9,26 @@ export class SpotifyService {
 
     // promessa de retornar uma string (url) nao é garantido retorno .
     async obterUrlLogin(): Promise<string> {
-        // pega da pasta enviroment a lista spotifyConfiguration o valor authEndpoint
+
+        // pega da pasta enviroment a lista SpotifyConfiguration o valor authEndpoint
+
+        const codigoAleatorio = this.gerarCodigoAleatorio();
         const authPoint = `${SpotifyConfiguration.authEndpoint}?`;
+        const clientId = `client_id=${SpotifyConfiguration.clientId}&`;
+        const urlRedirect = `redirect_url=${SpotifyConfiguration.redirectUrl}&`;
+        const scopes = `scope=${SpotifyConfiguration.scopes.join('%20')}`;
+        const codeChallengeMethod = 'code_challenge_method=S256';
+        const codeChallengeParam = 'code_challenge' + codigoAleatorio + '&';
+        const responseType = 'response_type=code';
+        return `${authPoint}${clientId}${urlRedirect}${scopes}${codeChallengeMethod}${codeChallengeParam}${responseType}`;
+    }
+
+
+    async gerarCodigoAleatorio() {
+        const codigoVerificador = '';
+        const codChallenge = '';
+
+        return codChallenge;
     }
 
     constructor() {
