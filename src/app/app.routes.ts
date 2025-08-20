75a5e6709d79c23259524e21f8218fa5f529c6ba
FireWater11 <luidi.augusto1234@gmail.com>
2025-08-19 21:33:29 -0300

    commit tudo funcionando criando site inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '', // se a url tiver vazia, automaticamente vai para a url /login
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
     path: 'login',
     loadChildren: () => import('./pages/login/login.routes').then(rota => rota.routes)
   }
